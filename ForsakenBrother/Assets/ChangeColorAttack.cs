a39d6a9a022563f78cf30584a2028e9575ca20b4
Dhanish Mouhamad <d.mouhamad@se18.qmul.ac.uk>
2020-10-30 11:14:21 +0000

    Powerup
    works fine.

---
+++ ForsakenBrother/Assets/ChangeColorAttack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorAttack : MonoBehaviour
+{
+    public Color spriteColor = Color.white;
+    
+    SpriteRenderer rend;
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        rend.material.color = spriteColor;
+
+
+    }
+
+    
+
+
+}
